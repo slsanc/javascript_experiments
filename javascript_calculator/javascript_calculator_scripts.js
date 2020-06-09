867d5d88f06441f45d3493b40dbd9c2da21934f3
Scott Sanchez <scottlowellsanchez@gmail.com>
2020-06-09 14:01:57 -0500

    Improved functionality and look of calculator app

--- javascript_calculator/javascript_calculator_scripts.js
+++ javascript_calculator/javascript_calculator_scripts.js
@@ -2,41 +2,43 @@ function pushNumber(n){
     if (window.currentOperation.operatorJustPushed){
         document.getElementById("numberDisplay").innerText = "";
     }
+
     window.currentOperation.operatorJustPushed = false;
     document.getElementById("numberDisplay").innerText += n;
 }
 
 function pushDecimal(){
     if (!document.getElementById("numberDisplay").innerText.includes('.')){
+        if (window.currentOperation.operatorJustPushed){
+            document.getElementById("numberDisplay").innerText = "0";
+        }
         document.getElementById("numberDisplay").innerText += '.'
     }
+
+    window.currentOperation.operatorJustPushed = false;
 }
 
 function pushOperator(operator){
-    pushEquals();
+
+    if(!window.currentOperation.operatorJustPushed){
+        if(window.currentOperation.operator == '+'){
+            window.currentOperation.num1 += Number(document.getElementById("numberDisplay").innerText);
+        }
+        else if(window.currentOperation.operator == '-'){
+            window.currentOperation.num1 -= Number(document.getElementById("numberDisplay").innerText);
+        }
+        else if(window.currentOperation.operator == '*'){
+            window.currentOperation.num1 *= Number(document.getElementById("numberDisplay").innerText);
+        }
+        else if(window.currentOperation.operator == '/'){
+            window.currentOperation.num1 /= Number(document.getElementById("numberDisplay").innerText);
+        }
+        else if(window.currentOperation.operator == '='){
+            window.currentOperation.num1 = Number(document.getElementById("numberDisplay").innerText);
+        }
+    }
+
     window.currentOperation.operator = operator;
-}
-
-function pushEquals(){
-
-    if(window.currentOperation.operator == '+'){
-        window.currentOperation.num1 += Number(document.getElementById("numberDisplay").innerText);
-    }
-    else if(window.currentOperation.operator == '-'){
-        window.currentOperation.num1 -= Number(document.getElementById("numberDisplay").innerText);
-    }
-    else if(window.currentOperation.operator == '*'){
-        window.currentOperation.num1 *= Number(document.getElementById("numberDisplay").innerText);
-    }
-    else if(window.currentOperation.operator == '/'){
-        window.currentOperation.num1 /= Number(document.getElementById("numberDisplay").innerText);
-    }
-    else if(window.currentOperation.operator == '='){
-        window.currentOperation.num1 = Number(document.getElementById("numberDisplay").innerText);
-    }
-
-    window.currentOperation.operator = '=';
-
     document.getElementById("numberDisplay").innerText = window.currentOperation.num1;
     window.currentOperation.operatorJustPushed = true;
 }
